54f15d7606b7c818fc1a90d9ef17888745d0803a
machine <analyticrye@gmail.com>
2020-03-29 16:51:00 -0400

    stream components created and setup with router

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,28 @@
 import React from "react";
 
+import { HashRouter, Route } from "react-router-dom";
+
+import StreamList from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamShow from "./streams/StreamShow";
+
+import Header from "./Header";
+
 const App = () => {
   return (
-    <div>
-      <p>App Working</p>
+    <div className="ui container">
+      <HashRouter>
+        <div>
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+        </div>
+      </HashRouter>
     </div>
   );
 };
